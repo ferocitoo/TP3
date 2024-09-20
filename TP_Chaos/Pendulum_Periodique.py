3209a69af26d45377c2c5d6937b6a8f485ba836e
Ciciss1 <160743697+Ciciss1@users.noreply.github.com>
2024-09-20 14:46:06 +0000

    a

--- TP_Chaos/Pendulum_Periodique.py
+++ TP_Chaos/Pendulum_Periodique.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 module_name = "utils_v2"
-file_path = "/workspaces/TP-Chaos/utils_v2.py"
+file_path = "/workspaces/TP3/utils_v2.py"
 
 spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
@@ -21,82 +21,125 @@ import pandas as pd
 
 
 #Import the data
-C1_C0_S1 = pd.read_csv("TP Chaos/Datas/Config1_Cond0_Simul1.csv", delimiter=';', decimal=',')
-C1_C0_S1_t=C1_C0_S1.iloc[:,0]
-C1_C0_S1_theta=C1_C0_S1.iloc[:,1]
-C1_C0_S1_thetadot=C1_C0_S1.iloc[:,2]
-
-C1_C1_S1 = pd.read_csv("TP Chaos/Datas/Config1_Cond1_Simul1.csv", delimiter=';', decimal=',')
-C1_C1_S1_t=C1_C1_S1.iloc[:,0]
-C1_C1_S1_theta=C1_C1_S1.iloc[:,1]
-C1_C1_S1_thetadot=C1_C1_S1.iloc[:,2]
-
-C1_C1_S2 = pd.read_csv("TP Chaos/Datas/Config1_Cond1_Simul2.csv", delimiter=';', decimal=',')
-C1_C1_S2_t=C1_C1_S2.iloc[:,0]
-C1_C1_S2_theta=C1_C1_S2.iloc[:,1]
-C1_C1_S2_thetadot=C1_C1_S2.iloc[:,2]
-
-C1_C1_S3 = pd.read_csv("TP Chaos/Datas/Config1_Cond1_Simul3.csv", delimiter=';', decimal=',')
-C1_C1_S3_t=C1_C1_S3.iloc[:,0]
-C1_C1_S3_theta=C1_C1_S3.iloc[:,1]
-C1_C1_S3_thetadot=C1_C1_S3.iloc[:,2]
-
-C1_C1_S4 = pd.read_csv("TP Chaos/Datas/Config1_Cond1_Simul4.csv", delimiter=';', decimal=',')
-C1_C1_S4_t=C1_C1_S4.iloc[:,0]
-C1_C1_S4_theta=C1_C1_S4.iloc[:,1]
-C1_C1_S4_thetadot=C1_C1_S4.iloc[:,2]
-
-C5_C0_S1 = pd.read_csv("TP Chaos/Datas/Config5_Cond0_Simul1.csv", delimiter=';', decimal=',')
-C5_C0_S1_t=C5_C0_S1.iloc[:,0]
-C5_C0_S1_theta=C5_C0_S1.iloc[:,1]
-C5_C0_S1_thetadot=C5_C0_S1.iloc[:,2]
-
-C5_C0_S2 = pd.read_csv("TP Chaos/Datas/Config5_Cond0_Simul2.csv", delimiter=';', decimal=',')
-C5_C0_S2_t=C5_C0_S2.iloc[:,0]
-C5_C0_S2_theta=C5_C0_S2.iloc[:,1]
-C5_C0_S2_thetadot=C5_C0_S2.iloc[:,2]
-
-C5_C1_S1 = pd.read_csv("TP Chaos/Datas/Config5_Cond1_Simul1.csv", delimiter=';', decimal=',')
-C5_C1_S1_t=C5_C1_S1.iloc[:,0]
-C5_C1_S1_theta=C5_C1_S1.iloc[:,1]   
-C5_C1_S1_thetadot=C5_C1_S1.iloc[:,2]
-
-C5_C1_S2 = pd.read_csv("TP Chaos/Datas/Config5_Cond1_Simul2.csv", delimiter=';', decimal=',')
-C5_C1_S2_t=C5_C1_S2.iloc[:,0]
-C5_C1_S2_theta=C5_C1_S2.iloc[:,1]
-C5_C1_S2_thetadot=C5_C1_S2.iloc[:,2]
-
-C5_C2_S1 = pd.read_csv("TP Chaos/Datas/Config5_Cond2_Simul1.csv", delimiter=';', decimal=',')
-C5_C2_S1_t=C5_C2_S1.iloc[:,0]
-C5_C2_S1_theta=C5_C2_S1.iloc[:,1]
-C5_C2_S1_thetadot=C5_C2_S1.iloc[:,2]
-
-C5_C2_S2 = pd.read_csv("TP Chaos/Datas/Config5_Cond2_Simul2.csv", delimiter=';', decimal=',')
-C5_C2_S2_t=C5_C2_S2.iloc[:,0]
-C5_C2_S2_theta=C5_C2_S2.iloc[:,1]
-C5_C2_S2_thetadot=C5_C2_S2.iloc[:,2]
-
-C6_C1_S1 = pd.read_csv("TP Chaos/Datas/Config6_Cond1_Simul1.csv", delimiter=';', decimal=',')
-C6_C1_S1_t=C6_C1_S1.iloc[:,0]
-C6_C1_S1_theta=C6_C1_S1.iloc[:,1]
-C6_C1_S1_thetadot=C6_C1_S1.iloc[:,2]
-
-C6_C1_S2 = pd.read_csv("TP Chaos/Datas/Config6_Cond1_Simul2.csv", delimiter=';', decimal=',')
-C6_C1_S2_t=C6_C1_S2.iloc[:,0]
-C6_C1_S2_theta=C6_C1_S2.iloc[:,1]
-C6_C1_S2_thetadot=C6_C1_S2.iloc[:,2]
-
-C7_Droite = pd.read_csv("TP Chaos/Datas/Config7_Droite.csv", delimiter=';', decimal=',')
+C7_Droite = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config7_Droite.csv", delimiter=';', decimal=',')
 C7_Droite_t=C7_Droite.iloc[:,0]
 C7_Droite_theta=C7_Droite.iloc[:,1]
 C7_Droite_thetadot=C7_Droite.iloc[:,2]
 
-C7_Gauche = pd.read_csv("TP Chaos/Datas/Config7_Gauche.csv", delimiter=';', decimal=',')
+C7_Gauche = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config7_Gauche.csv", delimiter=';', decimal=',')
 C7_Gauche_t=C7_Gauche.iloc[:,0]
 C7_Gauche_theta=C7_Gauche.iloc[:,1]
 C7_Gauche_thetadot=C7_Gauche.iloc[:,2]
 
-C7_GaucheDroite = pd.read_csv("TP Chaos/Datas/Config7_GaucheDroite.csv", delimiter=';', decimal=',')
+C7_GaucheDroite = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config7_GaucheDroite.csv", delimiter=';', decimal=',')
 C7_GaucheDroite_t=C7_GaucheDroite.iloc[:,0]
 C7_GaucheDroite_theta=C7_GaucheDroite.iloc[:,1]
 C7_GaucheDroite_thetadot=C7_GaucheDroite.iloc[:,2]
+
+C8_C1_S1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config8_Cond1_Simul1.csv", delimiter=';', decimal=',')
+C8_C1_S1_t=C8_C1_S1.iloc[:,0]
+C8_C1_S1_theta=C8_C1_S1.iloc[:,1]
+C8_C1_S1_thetadot=C8_C1_S1.iloc[:,2]
+
+C8_C1_S2 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config8_Cond1_Simul2.csv", delimiter=';', decimal=',')
+C8_C1_S2_t=C8_C1_S2.iloc[:,0]
+C8_C1_S2_theta=C8_C1_S2.iloc[:,1]
+C8_C1_S2_thetadot=C8_C1_S2.iloc[:,2]
+
+C8_C2_S1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config8_Cond2_Simul1.csv", delimiter=';', decimal=',')
+C8_C2_S1_t=C8_C2_S1.iloc[:,0]
+C8_C2_S1_theta=C8_C2_S1.iloc[:,1]
+C8_C2_S1_thetadot=C8_C2_S1.iloc[:,2]
+
+C8_C3_S1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas/Config8_Cond3_Simul1.csv", delimiter=';', decimal=',')
+C8_C3_S1_t=C8_C3_S1.iloc[:,0]
+C8_C3_S1_theta=C8_C3_S1.iloc[:,1]
+C8_C3_S1_thetadot=C8_C3_S1.iloc[:,2]
+
+
+#position
+xlabel = "Time [s]"
+ylabel = r"\theta" + " [V]"
+ax,fig = u.create_figure_and_apply_format((8,6),xlabel=xlabel, ylabel=ylabel)
+
+ax.plot(C7_Droite_t,C7_Droite_theta, label="Droite", color="blue")
+ax.plot(C7_Gauche_t,C7_Gauche_theta, label="Gauche", color="red")
+ax.plot(C7_GaucheDroite_t,C7_GaucheDroite_theta, label="GaucheDroite", color="green")
+
+timerange = [-2,20]
+ax.set_xlim(timerange)
+
+u.set_legend_properties(ax,fontsize=18)
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/Config7_position.pdf")
+
+
+#angular speed
+xlabel = "Time [s]"
+ylabel = r"\dot{\theta}" + " [rad]"
+ax,fig = u.create_figure_and_apply_format((8,6),xlabel=xlabel, ylabel=ylabel)
+
+ax.plot(C7_Droite_t,C7_Droite_thetadot, label="Droite", color="blue")
+ax.plot(C7_Gauche_t,C7_Gauche_thetadot, label="Gauche", color="red")
+ax.plot(C7_GaucheDroite_t,C7_GaucheDroite_thetadot, label="GaucheDroite", color="green")
+
+timerange = [-2,20]
+ax.set_xlim(timerange)
+
+u.set_legend_properties(ax,fontsize=18)
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/Config7_angul_speed.pdf")
+
+
+#phase space
+xlabel = r"\dot{\theta}" + " [rad]"
+ylabel = r"\theta" + " [V]"
+ax,fig = u.create_figure_and_apply_format((8,6),xlabel=xlabel, ylabel=ylabel)
+
+ax.plot(C7_Droite_thetadot,C7_Droite_theta, label="Droite", color="blue")
+ax.plot(C7_Gauche_thetadot,C7_Gauche_theta, label="Gauche", color="red")
+ax.plot(C7_GaucheDroite_thetadot,C7_GaucheDroite_theta, label="GaucheDroite", color="green")
+
+u.set_legend_properties(ax,fontsize=18)
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/Config7_phase_space.pdf")
+
+
+#spectral analysis
+xlabel = "Frequency [Hz]"
+ylabel = "Amplitude"
+ax,fig = u.create_figure_and_apply_format((8,6),xlabel=xlabel, ylabel=ylabel)
+
+fs = 1/(C7_Droite_t[1]-C7_Droite_t[0])
+
+n1 = len(C7_Droite_theta)
+n2 = len(C7_Gauche_theta)
+n3 = len(C7_GaucheDroite_theta)
+
+#FFT
+theta_demeaned1 =  C7_Droite_theta - np.mean(C7_Droite_theta)
+fft_values1 = np.fft.fft(theta_demeaned1)
+frequencies1 = np.fft.fftfreq(n1, d=1/fs)
+
+positive_freqs1 = frequencies1[:n1//2]
+positive_fft1 = np.abs(fft_values1[:n1//2])
+
+theta_demeaned2 = C7_Gauche_theta - np.mean(C7_Gauche_theta)
+fft_values2 = np.fft.fft(theta_demeaned2)
+frequencies2 = np.fft.fftfreq(n2, d=1/fs)
+
+positive_freqs2 = frequencies2[:n2//2]
+positive_fft2 = np.abs(fft_values2[:n2//2])
+
+theta_demeaned3 = C7_GaucheDroite_theta - np.mean(C7_GaucheDroite_theta)
+fft_values3 = np.fft.fft(theta_demeaned3)
+frequencies3 = np.fft.fftfreq(n3, d=1/fs)
+
+positive_freqs3 = frequencies3[:n3//2]
+positive_fft3 = np.abs(fft_values3[:n3//2])
+
+ax.plot(positive_freqs1, positive_fft1, color="blue",label="Exp. 1")
+ax.plot(positive_freqs2, positive_fft2, color="red",label="Exp. 2")
+ax.plot(positive_freqs3, positive_fft3, color="green",label="Exp. 3")
+
+freq_range=[0,1]
+ax.set_xlim(freq_range)
+u.set_legend_properties(ax,fontsize=18)
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/Config7_spectral_analysis.pdf")
