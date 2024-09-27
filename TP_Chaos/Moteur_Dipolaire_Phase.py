4a4b8866a073357941a86803e462012d14d4e462
Ciciss1 <160743697+Ciciss1@users.noreply.github.com>
2024-09-27 11:58:41 +0000

    a

--- TP_Chaos/Moteur_Dipolaire_Phase.py
+++ TP_Chaos/Moteur_Dipolaire_Phase.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 module_name = "utils_v2"
-file_path = "/workspaces/TP-Chaos/utils_v2.py"
+file_path = "/workspaces/TP3/utils_v2.py"
 
 spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
@@ -22,49 +22,49 @@ import pandas as pd
 
 #Import the data
 #----config1-----#
-C1 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C1_t=C1.iloc[:,0]
 C1_theta=C1.iloc[:,1]
 C1_thetadot=C1.iloc[:,2]
 
 #----config2-----#
 #3 périodes mieux
-C2_1 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_2_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C2_1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_2_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C2_1_t=C2_1.iloc[:,0]
 C2_1_theta=C2_1.iloc[:,1]
 C2_1_thetadot=C2_1.iloc[:,2]
 
 #3 pértiodes inversé
-C2_2 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_2_2.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C2_2 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_2_2.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C2_2_t=C2_2.iloc[:,0]
 C2_2_theta=C2_2.iloc[:,1]
 C2_2_thetadot=C2_2.iloc[:,2]
 
 #1 période
-C2_3 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_2_3.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C2_3 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_2_3.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C2_3_t=C2_3.iloc[:,0]
 C2_3_theta=C2_3.iloc[:,1]
 C2_3_thetadot=C2_3.iloc[:,2]
 
 #----config3-----#
-C3_1 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_3_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C3_1 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_3_1.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C3_1_t=C3_1.iloc[:,0]
 C3_1_theta=C3_1.iloc[:,1]
 C3_1_thetadot=C3_1.iloc[:,2]
 
-C3_2 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_3_2.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C3_2 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_3_2.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C3_2_t=C3_2.iloc[:,0]
 C3_2_theta=C3_2.iloc[:,1]
 C3_2_thetadot=C3_2.iloc[:,2]
 
-C3_3 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_3_3.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C3_3 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_3_3.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C3_3_t=C3_3.iloc[:,0]
 C3_3_theta=C3_3.iloc[:,1]
 C3_3_thetadot=C3_3.iloc[:,2]
 
 #----config4-----#
 #1 période
-C4 = pd.read_csv("TP_Chaos/Datas_Moteur/Config_4.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
+C4 = pd.read_csv("/workspaces/TP3/TP_Chaos/Datas_Moteur/Config_4.txt", skiprows=6, usecols=[0, 1, 2], delimiter='\t', decimal = ",")  # Adjust delimiter if necessary
 C4_t=C4.iloc[:,0]
 C4_theta=C4.iloc[:,1]
 C4_thetadot=C4.iloc[:,2]
@@ -87,7 +87,7 @@ ax,fig = u.create_figure_and_apply_format((8,6),xlabel=xlabel, ylabel=ylabel)
 ax.plot(theta,thetadot,color="black")
 
 u.set_legend_properties(ax,fontsize=18)
-fig.savefig("TP_Chaos/Figures/Config1_phase_space.pdf")
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/DP_Config1_phase_space.pdf")
 
 #spectral analysis
 xlabel = "Frequency [Hz]"
@@ -111,7 +111,7 @@ ax.plot(positive_freqs, positive_fft, color="black")
 freq_range=[0,1]
 ax.set_xlim(freq_range)
 u.set_legend_properties(ax,fontsize=18)
-fig.savefig("TP_Chaos/Figures/Config1_spectral_analysis.pdf")
+fig.savefig("/workspaces/TP3/TP_Chaos/Figures/DP_Config1_spectral_analysis.pdf")
 
 
 
